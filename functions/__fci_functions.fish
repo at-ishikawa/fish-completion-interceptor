4e586f9eb3a6d9cf51977344077a40adf269addb
A. Ishikawa <10303962+at-ishikawa@users.noreply.github.com>
2024-04-03 09:30:41 -0700

    feature/github cli: enable to switch the PR lists of the user created and user needs to review

--- functions/__fci_functions.fish
+++ functions/__fci_functions.fish
@@ -3,10 +3,23 @@ function __fci_functions \
 
     # fzf during the pipe doesn't work even with a redirection of stdout to stderr and stderr to stdout
     function __fci_fzf
-        argparse multi "header-lines=?" "preview=?" "query=?" -- $argv
+        argparse multi \
+            "header=?" \
+            "header-lines=?" \
+            "preview=?" \
+            "query=?" \
+            "bind=+" \
+            "prompt=?" \
+            -- $argv
         set -l fzf_options
         if set -ql _flag_multi
             set -a fzf_options --multi
+        end
+        if [ -n "$_flag_prompt" ]
+            set -a fzf_options "--prompt=$_flag_prompt"
+        end
+        if [ -n "$_flag_header" ]
+            set -a fzf_options "--header=$_flag_header"
         end
         if [ -n "$_flag_header_lines" ]; and [ $_flag_header_lines -gt 0 ]
             set -a fzf_options "--header-lines=$_flag_header_lines"
@@ -16,6 +29,11 @@ function __fci_functions \
         end
         if [ -n "$_flag_query" ]
             set -a fzf_options "--query=$_flag_query"
+        end
+        if set -ql _flag_bind
+            for bind in $_flag_bind
+                set -a fzf_options "--bind=$bind"
+            end
         end
         for option in $FISH_COMPLETION_INTERCEPTOR_FZF_OPTIONS
             set -a fzf_options $option
--- functions/fci_plugin_gh_fzf.fish
+++ functions/fci_plugin_gh_fzf.fish
@@ -1,36 +1,27 @@
 __fci_functions
 
-function __fci_plugin_gh_fzf_gh_cli
-
-    argparse "repo=?" -- $argv
-    set -l cli_options
-    if [ -n "$_flag_repo" ]
-        set -a cli_options "--repo=$_flag_repo"
-    end
-
-    $__FCI_PLUGIN_GH_FZF_GH_CLI pr list $cli_options
-    return $status
-end
-
 function __fci_plugin_gh_fzf_pr_list
 
     argparse "repo=?" "query=?" -- $argv
     set -l fzf_preview_command "gh pr view {1}"
+    set -l cli_options
     if [ -n $_flag_repo ]
+        set a cli_options "--repo=$_flag_repo"
         set -a fzf_preview_command "--repo=$_flag_repo"
     end
 
-    set -l cli_result (__fci_plugin_gh_fzf_gh_cli "--repo=$_flag_repo" 2>&1)
-    set -l cli_status $status
-    if [ $cli_status -ne 0 ]
-        echo -n -e "$cli_result"
-        return $cli_status
-    end
-
-    string split "\n" -- $cli_result |
-        __fci_fzf "--query=$_flag_query" "--preview=$fzf_preview_command" |
+    # https://github.com/junegunn/fzf?tab=readme-ov-file#2-switch-between-sources-by-pressing-ctrl-d-or-ctrl-f
+    # https://github.com/junegunn/fzf/issues/2423#issuecomment-814577418
+    FZF_DEFAULT_COMMAND="$__FCI_PLUGIN_GH_FZF_GH_CLI pr list --search 'state:open author:@me' $cli_options" \
+        __fci_fzf \
+        "--prompt=Your PRs> " \
+        "--bind=ctrl-s:change-prompt(Your PRs> )+reload($FZF_DEFAULT_COMMAND)" \
+        "--bind=ctrl-r:change-prompt(Other PRs> )+reload($__FCI_PLUGIN_GH_FZF_GH_CLI pr list --search 'state:open review:required review-requested:@me' $clip_options)" \
+        "--header=Ctrl-S: Show your PRs. Ctrl-R: Show other PRs you are requested to be reviewed" \
+        "--query=$_flag_query" \
+        "--preview=$fzf_preview_command" |
         awk '{ print $1 }'
-    return $pipestatus[2]
+    return $pipestatus[1]
 end
 
 function fci_plugin_gh_fzf \
