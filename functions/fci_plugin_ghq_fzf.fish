1490609fdf4aea4fb96f7e639654ecf1834c121e
A. Ishikawa <10303962+at-ishikawa@users.noreply.github.com>
2024-12-20 18:57:58 -0800

    fix an error

--- functions/fci_plugin_ghq_fzf.fish
+++ functions/fci_plugin_ghq_fzf.fish
@@ -12,11 +12,10 @@ function fci_plugin_ghq_fzf --description "The plugin for ghq"
     set -l subcommand $argv[2]
 
     switch $subcommand
-    case get
-        argparse --ignore-unknown -- $argv
-        set -l query $argv[-1]
-        __fci_plugin_gh_fzf_repo_list "--query=$query"
-        return $status
+        case get
+            set -l query $argv[-1]
+            __fci_plugin_gh_fzf_repo_list "--query=$query"
+            return $status
     end
 
     return 0
